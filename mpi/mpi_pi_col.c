5bf3583bc464e03136b6c96cf5d69cf1a60db974
João Marcelo <jmarcelo.alencar@gmail.com>
2014-02-07 14:38:26 -0300

    mpi_pi_col

--- mpi/mpi_pi_col.c
+++ mpi/mpi_pi_col.c
@@ -4,8 +4,8 @@
 #include "mpi.h"
 
 // Número total de interações
-#define N 1000000
-#define RAIO 10
+#define N 1000
+#define RAIO 5
 
 // Função que executa o trabalho. Está aqui por organização. 
 long generatePoints(long my_n) {
@@ -32,7 +32,8 @@ long generatePoints(long my_n) {
 int main(int argc, char *argv[]) {
     int rank, size;
     long my_n;
-    long my_circle = 0;
+    long circle = 0;    
+
     MPI_Status status;
     
     MPI_Init(&argc, &argv);
@@ -41,41 +42,19 @@ int main(int argc, char *argv[]) {
    
     // Quanto cada processo executará.
     my_n = N / size;
-   
-    if (rank == 0) {
-        // O processo zero distribui o valor de my_n e acumula a contribuição de cada um em circle.
-        int i;
-        int circle = 0;
  
-        // Distribuindo valor de my_n.
-        for (i = 1; i < size; i++) {
-            MPI_Send(&my_n, 1, MPI_LONG, i, 0, MPI_COMM_WORLD);
-        }
-        
-        // Calcula a sua parte.
-        circle = generatePoints(my_n);
+    // Distribuindo valor de my_n.
+    MPI_Bcast(&my_n, 1, MPI_LONG, 0, MPI_COMM_WORLD);       
+  
+    // Calcula a sua parte.
+    circle = generatePoints(my_n);
 
-        // Acumula com as dos outros.
-        for (i = 1; i < size; i++) {
-            long temp;
-            MPI_Recv(&temp, 1, MPI_LONG, i, 0, MPI_COMM_WORLD, &status);
-            circle += temp;
-        }
-        
-        printf("%.5f\n", ((double) 4 * circle) / N);              
- 
-    } else {
-        // Recebe de zero o valor de my_n, calcula os pontos e retorna para o processo 0.
-
-        // Recebe de zero o valor de my_n
-        MPI_Recv(&my_n, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD, &status);
-        
-	// Calcula os pontos
-        my_circle = generatePoints(my_n);
-
-        // Envia para 0 o valor calculado.
-        MPI_Send(&my_circle, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
-    }
+    // Acumula com as dos outros.
+    long temp;
+    MPI_Reduce(&circle, &temp, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    circle = temp;
+            
+    if (rank == 0)  printf("%.5f\n", ((double) 4 * circle) / N);              
 
     MPI_Finalize();
     return 0;
