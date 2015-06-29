adc004cc74a74b2164a1e8a28fad885f57640cde
jmhal <jmarcelo.alencar@gmail.com>
2015-06-29 12:01:07 -0300

    Atualizando o script de instalar o backend. Falta colocar as etapas intermediárias, Mysql.NET e MPI.NET.

--- hpe/scripts/instalar_backend.sh
+++ hpe/scripts/instalar_backend.sh
@@ -2,11 +2,13 @@
 # Script para instalar o HPE BackEnd
 
 # Variáveis 
+HPE_USER=
+HPE_USER_PASSWD=
 DATABASE_USER="hashmodel";
 DATABASE_PASSWORD="1566hpe";
-MPI_NET_DIR=
+MPI_NET_DIR=/opt/mpi.net/lib
 MPI_NET_URL=
-MYSQL_NET_DIR=
+MYSQL_NET_DIR=/opt/mysql-connector-net/v2.0
 MYSQL_NET_URL=
 BACKEND_DIR="/home/jmhal/hash-programming-environment/HPE_BackEnd";
 CACHE_DIR="/home/jmhal/hpe/cache";
@@ -19,8 +21,8 @@ export DEBIAN_FRONTEND=noninteractive;
 apt-get update -q;
 
 # Pacotes básicos de compilação e ambiente Mono
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \\
-debconf-utils libtool automake subversion git g++ libopenmpi-dev openmpi-common openmpi-bin mono-runtime \\
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+debconf-utils libtool automake subversion git g++ libopenmpi-dev openmpi-common openmpi-bin mono-runtime \
 mono-runtime-common mono-devel mono-xsp2 mono-xsp2 mono-2.0-service mono-4.0-service; 
 
 # Instalação do mysql-server
@@ -33,6 +35,40 @@ grant all privileges on hashmodel.* to '${DATABASE_USER}'@'%' identified by '${D
 grant all privileges on hashmodel.* to '${DATABASE_USER}'@'localhost' identified by '${DATABASE_PASSWORD}';
 EOF
 
+## Compilar HPE_BackEnd ##
+cd ~${HPE_USER};
+
+# Baixar o código e configurar os diretórios
+git clone https://github.com/UFC-MDCC-HPC/Hash-Programming-Environment;
+cd Hash-Programming-Environment/HPE_BackEnd/;
+mkdir bin;
+
+# Compilar DGAC
+sed -i s/heron/${HPE_USER}/ DGAC/DGAC.cs;
+sed -i s/narf@jchf9200/${HPE_USER_PASSWD}/ DGAC/DGAC.cs;
+sed -i s/configfile/app/ DGAC/DGAC.cs;
+sed -i s/worker.launch.config.9/worker.launch.config/ DGAC/DGAC.cs;
+sed -i s#/usr/lib/mono/MySql.Data#${MYSQL_NET_DIR}# make_dgac.rsp;
+sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}# make_dgac.rsp;
+dmcs @make_dgac.rsp;
+gacutil -i bin/DGAC.dll;
+
+# Compilar worker
+sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}# make_worker.rsp;
+sed -i s#/usr/lib/mono/DGAC#${BACKEND_DIR}/bin# make_worker.rsp;
+dmcs @make_worker.rsp;
+
+# Compilar manager
+sed -i s#-r:DGAC#-r:${BACKEND_DIR}/bin/DGAC.dll# make_manager.rsp;
+dmcs @make_manager.rsp;
+
+# Compilar WS
+sed -i s#/usr/lib/mono/DGAC#${BACKEND_DIR}/bin# make_backendws.rsp;
+dmcs @make_backendws.rsp;
+
+# Compilar run_app
+sed -i s#-r:DGAC#-r:${BACKEND_DIR}/bin/DGAC.dll# make_run_app.rsp;
+dmcs @make_run_app.rsp;
 
 # Apagar o Banco de Dados e recarregar o esquema
 #mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} hashmodel<<EOF
