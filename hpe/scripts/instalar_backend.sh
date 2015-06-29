5eb8559129fab10d5bfeaa5ceae5b110a7050880
jmhal <jmarcelo.alencar@gmail.com>
2015-06-29 16:20:35 -0300

    Falta testar e criar os arquivos hpe.backend.properties, /etc/hpe_nodes e external_references.xml

--- hpe/scripts/instalar_backend.sh
+++ hpe/scripts/instalar_backend.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Script para instalar o HPE BackEnd
 
-# Variáveis 
+# Variáveis que precisam ser preenchidas!
 HPE_USER=
 HPE_USER_PASSWD=
 DATABASE_USER="hashmodel";
 DATABASE_PASSWORD="1566hpe";
-MPI_NET_DIR=/opt/mpi.net/lib
-MPI_NET_URL=
-MYSQL_NET_DIR=/opt/mysql-connector-net/v2.0
-MYSQL_NET_URL=
+MPI_NET_DIR="/opt/mpi.net";
+MPI_NET_URL="https://github.com/jmhal/MPI.NET";
+MYSQL_NET_DIR="/opt/mysql-connector-net";
+MYSQL_NET_URL="https://github.com/jmhal/parallel/raw/master/hpe/mysql-connector-net-6.9.6-noinstall.zip";
 BACKEND_DIR="/home/jmhal/hash-programming-environment/HPE_BackEnd";
-CACHE_DIR="/home/jmhal/hpe/cache";
-WORK_DIR="/home/jmhal/hpe/work";
-PATH_DIR="/home/jmhal/hpe/path";
-UNIT_PATH_DIR="/home/jmhal/hpe/unit";
+CACHE_DIR="~${HPE_USER}/hpe/cache";
+WORK_DIR="~${HPE_USER}/hpe/work";
+PATH_DIR="~${HPE_USER}/hpe/path";
+UNIT_PATH_DIR="~${HPE_USER}/hpe/unit";
 
 ## Instalar os pacotes ##
 export DEBIAN_FRONTEND=noninteractive;
@@ -35,6 +35,64 @@ grant all privileges on hashmodel.* to '${DATABASE_USER}'@'%' identified by '${D
 grant all privileges on hashmodel.* to '${DATABASE_USER}'@'localhost' identified by '${DATABASE_PASSWORD}';
 EOF
 
+## Instalar MPI.NET ##
+mkdir ${MPI_NET_DIR};
+cd ~${HPE_USER};
+git clone ${MPI_NET_URL} mpi.net
+cd mpi.net;
+LOCAL_DIR=/opt/mpi.net;
+sh autogen.sh;
+./configure –prefix=${MPI_NET_DIR};
+make;
+make install;
+gacutil -i /opt/mpi.net/lib/MPI.dll;
+gacutil -i /opt/mpi.net/lib/MPIUtils.dll;
+
+# Testando MPI e MPI.NET
+wget -q https://raw.githubusercontent.com/jmhal/parallel/master/hpe/scripts/testes/TesteMPI.c;
+mpicc TesteMPI.c -o TesteMPI;
+test=$(mpirun -np 2 ./TesteMPI | grep OK | wc -l);
+if [ $test -eq 2 ]; then
+   echo "MPI OK";
+else 
+   echo "MPI NOT OK";
+   exit 1;
+fi
+
+wget -q https://raw.githubusercontent.com/jmhal/parallel/master/hpe/scripts/testes/TesteMPINET.cs; 
+dmcs -warn:0 -r:${MPI_NET_DIR}/lib/MPI.dll TesteMPINET.cs;
+export LD_LIBRARY_PATH=${MPI_NET_DIR};
+test=$(mpirun -np 2 mono ./TesteMPINET.exe | grep OK | wc -l);
+if [ $test -eq 2 ]; then
+   echo "MPI.NET OK";
+else 
+   echo "MPI.NET NOT OK";
+   exit 1;
+fi
+rm -f TesteMPI TesteMPI.c TesteMPINET.*;
+
+## Instalar o MySQL.NET ##
+mkdir ${MYSQL_NET_DIR};
+cd ~${HPE_USER};
+wget -q ${MYSQL_NET_URL};
+unzip mysql-connector-net-6.9.6-noinstall.zip -d ${MYSQL_NET_DIR};
+gacutil -i ${MYSQL_NET_DIR}/v2.0/MySql.Data.dll;
+
+# Testando MySQL.NET
+cd ~${HPE_USER};
+wget -q https://raw.githubusercontent.com/jmhal/parallel/master/hpe/scripts/testes/TesteMYSQLNET.cs;
+sed -i s/usuario/${DATABASE_USER}/ TesteMYSQLNET.cs;
+sed -i s/senha/${DATABASE_PASSWORD}/ TesteMYSQLNET.cs;
+dmcs -r:System.Data.dll -r:/opt/mysql-connector-net/v2.0/MySql.Data.dll TesteMYSQLNET.cs;
+test=$(mono TesteMYSQLNET.exe | grep schema | wc -l);
+if [ $test -eq 1 ]; then
+   echo "MySQL.NET OK";
+else
+   echo "MySQL.NET NOT OK";
+   exit 1;
+fi
+rm -f TesteMYSQLNET.*;
+
 ## Compilar HPE_BackEnd ##
 cd ~${HPE_USER};
 
@@ -48,13 +106,13 @@ sed -i s/heron/${HPE_USER}/ DGAC/DGAC.cs;
 sed -i s/narf@jchf9200/${HPE_USER_PASSWD}/ DGAC/DGAC.cs;
 sed -i s/configfile/app/ DGAC/DGAC.cs;
 sed -i s/worker.launch.config.9/worker.launch.config/ DGAC/DGAC.cs;
-sed -i s#/usr/lib/mono/MySql.Data#${MYSQL_NET_DIR}# make_dgac.rsp;
-sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}# make_dgac.rsp;
+sed -i s#/usr/lib/mono/MySql.Data#${MYSQL_NET_DIR}/v2.0# make_dgac.rsp;
+sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}/lib# make_dgac.rsp;
 dmcs @make_dgac.rsp;
 gacutil -i bin/DGAC.dll;
 
 # Compilar worker
-sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}# make_worker.rsp;
+sed -i s#/usr/lib/mono/MPI#${MPI_NET_DIR}/lib# make_worker.rsp;
 sed -i s#/usr/lib/mono/DGAC#${BACKEND_DIR}/bin# make_worker.rsp;
 dmcs @make_worker.rsp;
 
@@ -70,39 +128,10 @@ dmcs @make_backendws.rsp;
 sed -i s#-r:DGAC#-r:${BACKEND_DIR}/bin/DGAC.dll# make_run_app.rsp;
 dmcs @make_run_app.rsp;
 
-# Apagar o Banco de Dados e recarregar o esquema
-#mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} hashmodel<<EOF
-#source ${BACKEND_DIR}/DGAC2.sql;
-#EOF
-
-# Compilar DGAC.dll
-#cd ${BACKEND_DIR};
-#gmcs @make_dgac.rsp;
-#echo "Informe a senha para instalação do DGACL.dll no GAC";
-#sudo gacutil -u DGAC;
-#sudo gacutil -i bin/DGAC.dll;
-
-# Compilar Manager
-#cd ${BACKEND_DIR};
-#gmcs @make_manager.rsp
-
-# Compilar Worker
-#cd ${BACKEND_DIR};
-#gmcs @make_worker.rsp 
-
-# Compilar WS
-#cd ${BACKEND_DIR};
-#gmcs @make_backendws.rsp 
-
-# Compular o hpe_run
-#cd ${BACKEND_DIR};
-#gmcs @make_run_app.rsp 
-
-# Limpar diretórios para novo deploy
-#echo "Forneça senha para apagar os diretórios.";
-#sudo rm -rf ${CACHE_DIR}/*;
-#sudo rm -rf ${WORK_DIR}/*;
-#sudo rm -rf ${PATH_DIR}/*;
-#sudo rm -rf ${UNIT_PATH_DIR}/*;
+# Criar diretórios auxiliares 
+mkdir -p ${CACHE_DIR};
+mkdir -p ${WORK_DIR};
+mkdir -p ${PATH_DIR};
+mkdir -p ${UNIT_PATH_DIR};
 
 
