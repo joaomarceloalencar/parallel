143860703d3d4242cf8d8032f6462dbf7b96c6d2
jmhalqxufc <jmhal@quixada.ufc.br>
2019-05-28 21:10:16 -0300

    Transformado em Multiplicação de Matrizes.

--- matriz/src/main.c
+++ matriz/src/main.c
@@ -4,55 +4,61 @@
 #include "matriz.h"
 
 int main(int argc, char *argv[]) {
-   // Nome do arquivo com a matriz.	
-   char *nomeArquivoMatriz = argv[1];
+   // Nome do arquivo com a matriz A.	
+   char *nomeArquivoMatrizA = argv[1];
 
-   // Nome do arquivo com o vetor.
-   char *nomeArquivoVetor = argv[2];
+   // Nome do arquivo com o matriz B.
+   char *nomeArquivoMatrizB = argv[2];
 
    // Nome do arquivo com o resultado.
-   char *nomeArquivoResultado = argv[3];
+   char *nomeArquivoMatrizResultado = argv[3];
 
-   FILE *arquivoMatriz = fopen(nomeArquivoMatriz, "r");
-   FILE *arquivoVetor = fopen(nomeArquivoVetor, "r");
-   if ((arquivoMatriz == NULL) || (arquivoVetor == NULL)) {
+   FILE *arquivoMatrizA = fopen(nomeArquivoMatrizA, "r");
+   FILE *arquivoMatrizB = fopen(nomeArquivoMatrizB, "r");
+   if ((arquivoMatrizA == NULL) || (arquivoMatrizB == NULL)) {
       printf("Erro ao abrir os arquivos.\n");
-      return 1;
+      return -1;
    }
 
-   Matriz matriz = lerMatriz(arquivoMatriz);
-   imprimirMatriz(matriz);
+   Matriz matrizA = lerMatriz(arquivoMatrizA);
+   imprimirMatriz(matrizA);
 
-   Matriz vetor = lerMatriz(arquivoVetor);
-   imprimirMatriz(vetor);
+   Matriz matrizB = lerMatriz(arquivoMatrizB);
+   imprimirMatriz(matrizA);
+
+   if (matrizA.m != matrizB.n) {
+      printf("Dimensões incompatíveis.\n");
+      return -1;
+   }
    
    // Realiza a multiplicação.
    Matriz resultado;
-   resultado.n = matriz.n;
-   resultado.m = vetor.m;
-   resultado.data = (float *) malloc(resultado.n * sizeof(float));
-   for (int i = 0; i < resultado.n; i++) resultado.data[i] = 0.0;
-   for (int i = 0; i < matriz.n; i++)
-      for (int j = 0; j < matriz.m; j++)
-         resultado.data[i] += matriz.data[i * matriz.m + j] * vetor.data[j];	      
+   resultado.n = matrizA.n;
+   resultado.m = matrizB.m;
+   resultado.data = (float *) malloc(resultado.m * resultado.n * sizeof(float));
+   for (int i = 0; i < resultado.n * resultado.m; i++) resultado.data[i] = 0.0;
+   for (int i = 0; i < matrizA.n; i++)
+      for (int j = 0; j < matrizB.m; j++)
+         for (int k = 0; k < matrizB.n; k++)
+            resultado.data[i * matrizB.m + j] += matrizA.data[i * matrizA.m + k] * matrizB.data[k * matrizB.m + j];	      
 
    // Guarda o resultado no arquivo.
-   FILE *arquivoResultado = fopen(nomeArquivoResultado, "w");
-   fprintf(arquivoResultado, "%d\n", resultado.n);
-   fprintf(arquivoResultado, "%d\n", resultado.m);
+   FILE *arquivoMatrizResultado = fopen(nomeArquivoMatrizResultado, "w");
+   fprintf(arquivoMatrizResultado, "%d\n", resultado.n);
+   fprintf(arquivoMatrizResultado, "%d\n", resultado.m);
    int i, j;
    for (i = 0; i < resultado.n; i++) {
       for(j = 0; j < resultado.m - 1; j++)
-         fprintf(arquivoResultado, "%0.2f:", resultado.data[i * resultado.m + j]);
-      fprintf(arquivoResultado, "%0.2f\n", resultado.data[i * resultado.m + j]);
+         fprintf(arquivoMatrizResultado, "%0.2f:", resultado.data[i * resultado.m + j]);
+      fprintf(arquivoMatrizResultado, "%0.2f\n", resultado.data[i * resultado.m + j]);
    }
  
-   free(matriz.data);
-   free(vetor.data);
+   free(matrizA.data);
+   free(matrizB.data);
    free(resultado.data);
-   fclose(arquivoMatriz);
-   fclose(arquivoVetor);
-   fclose(arquivoResultado);
+   fclose(arquivoMatrizA);
+   fclose(arquivoMatrizB);
+   fclose(arquivoMatrizResultado);
 
    return 0;
 }
