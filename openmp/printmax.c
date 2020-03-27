cbbf5781aeffdf729c3f080c35475b16bcfd15f7
Joao Marcelo <joao.marcelo@ufc.br>
2020-03-27 15:51:36 -0300

    Exemplo alterado em aula.

--- openmp/printmax.c
+++ openmp/printmax.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <omp.h>
 
 int main(int argc, char *argv[]) {
     int max;
     sscanf(argv[1], "%d", &max);
-    #pragma omp parallel for
-    for (int i = 1; i <= max; i++)
-        printf("%d: %d\n", omp_get_thread_num(), i);
+    #pragma omp parallel for schedule(runtime)
+    for (int i = 1; i <= max; i++) {
+        printf("%2d @ %d\n", i, omp_get_thread_num());
+        sleep(i<4?i+1:1);
+    }
     return 0;    
 }
