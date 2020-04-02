f3de0740dd71d8447d6f4f35c4c22e853597a7f1
Joao Marcelo <joao.marcelo@ufc.br>
2020-04-02 18:46:11 -0300

    Limpeza.

--- mpi/vector_sum/vector_sum.c
+++ mpi/vector_sum/vector_sum.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[]) {
    int rank, size, vetorTamanho;
+   
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
