d9600c112cae2084191f05c6bfa332f45884030b
João Marcelo Uchôa de Alencar <jmhal@oberon.local>
2022-06-10 19:24:32 -0300

    Removendo exemplos do Mono.

--- monoworkspace/MatMult/MatMult/bin/Debug/main.d
+++
@@ -1,3 +0,0 @@
-/home/jmhal/parallel/monoworkspace/MatMult/MatMult/bin/Debug/main.o: \
- /home/jmhal/parallel/monoworkspace/MatMult/MatMult/main.c \
- /home/jmhal/parallel/monoworkspace/MatMult/MatMult/SharedMemory.h
